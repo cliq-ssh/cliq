3e464e097e244d932a91a0d6a8557909e52bd60f
Jason Dean Lessenich <jasonlessenich@gmail.com>
2025-10-06 19:17:52 +0200

    added WidgetState to CliqButton & CliqIconButton

--- frontend/packages/cliq_ui/lib/src/theme/style.dart
+++ frontend/packages/cliq_ui/lib/src/theme/style.dart
@@ -5,11 +5,13 @@ final class CliqStyle {
   final BorderRadius borderRadius;
   final double borderWidth;
   final double verticalPagePadding;
+  final Duration animationDuration;
 
   const CliqStyle({
     this.borderRadius = const BorderRadius.all(Radius.circular(8)),
     this.borderWidth = 1,
     this.verticalPagePadding = 8.0,
+    this.animationDuration = const Duration(milliseconds: 200),
   });
 
   CliqStyle.inherit({
