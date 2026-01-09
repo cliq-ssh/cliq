ae3c15cc06d86aaefbc3b4d87d5bd9cad370c481
Denux <timon.klinkert@denux.dev>
2026-01-09 10:14:31 +0100

    fixed merge conflicts

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -97,7 +97,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
     // Flyway
-    val flywayVersion = "11.20.0"
+    val flywayVersion = "11.20.1"
     implementation("org.springframework.boot:spring-boot-starter-flyway")
     implementation("org.flywaydb:flyway-core:$flywayVersion")
     implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
