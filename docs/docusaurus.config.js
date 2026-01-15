bf3ca6af206836f6c122fc018e85faec75002766
Denux <timon.klinkert@denux.dev>
2026-01-15 23:56:43 +0100

    Clean up and fixes for the docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'http://localhost',
+  url: 'https://cliq-ssh.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
