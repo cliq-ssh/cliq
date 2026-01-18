a90299942dde4fd21c31ba060cb04777f6e02f8a
Jason Dean Lessenich <jasonlessenich@gmail.com>
2026-01-18 21:48:02 +0100

    some cleanup, use real text controllers instead of weird map mess

--- frontend/lib/shared/ui/create_or_edit_credential_form.dart
+++ frontend/lib/shared/ui/create_or_edit_credential_form.dart
@@ -1,3 +1,4 @@
+import 'package:cliq/shared/data/database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:forui/forui.dart';
@@ -13,12 +14,15 @@ enum _AllowedCredentialType {
     label: 'Password',
     icon: LucideIcons.rectangleEllipsis,
     singleInstance: true,
-  ),
-  key(type: .key, label: 'Key', icon: LucideIcons.keyRound);
+  )
+  // TODO: key(type: .key, label: 'Key', icon: LucideIcons.keyRound)
+  ;
 
   final CredentialType type;
   final String label;
   final IconData icon;
+
+  /// Whether only a single instance of this credential type is allowed.
   final bool singleInstance;
 
   const _AllowedCredentialType({
@@ -27,6 +31,31 @@ enum _AllowedCredentialType {
     required this.icon,
     this.singleInstance = false,
   });
+}
+
+final class _CredentialData {
+  final CredentialType type;
+  final TextEditingController dataController = TextEditingController();
+  final TextEditingController passphraseController = TextEditingController();
+
+  _CredentialData({
+    required this.type,
+    String? initialData,
+    String? initialPassphrase,
+  }) {
+    ;
+    if (initialData != null) {
+      dataController.text = initialData;
+    }
+    if (initialPassphrase != null) {
+      passphraseController.text = initialPassphrase;
+    }
+  }
+
+  void dispose() {
+    dataController.dispose();
+    passphraseController.dispose();
+  }
 }
 
 class CreateOrEditCredentialsForm extends HookConsumerWidget {
@@ -44,130 +73,126 @@ class CreateOrEditCredentialsForm extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final selectedCredentials =
-        useState<Map<int, (CredentialType, String, String?)>>({});
+    final selectedCredentials = useState<List<_CredentialData>>([]);
 
-    buildRemoveCredentialButton(int key) {
-      return FButton.icon(
-        style: FButtonStyle.destructive(),
-        onPress: () {
-          final updated = {...selectedCredentials.value};
-          updated.remove(key);
-          selectedCredentials.value = updated;
-        },
-        child: const Icon(LucideIcons.trash),
+    useEffect(() {
+      if (current != null) {
+        CliqDatabase.credentialService.findByIds(current!).then((creds) {
+          selectedCredentials.value = creds.map((c) {
+            return _CredentialData(
+              type: c.type,
+              initialData: c.type == .password ? c.password : c.key?.privatePem,
+              initialPassphrase: c.type == .key ? c.key?.passphrase : null,
+            );
+          }).toList();
+        });
+      }
+
+      return () {
+        for (final credential in selectedCredentials.value) {
+          credential.dispose();
+        }
+      };
+    }, []);
+
+    buildCredentialLabel(_CredentialData credential) {
+      final allowedType = _AllowedCredentialType.values.firstWhere(
+        (e) => e.type == credential.type,
       );
+
+      return Row(
+        spacing: 8,
+        mainAxisAlignment: .spaceBetween,
+        children: [
+          Text(allowedType.label),
+          FButton.icon(
+            style: FButtonStyle.ghost(),
+            onPress: () {
+              credential.dispose();
+              final updated = [...selectedCredentials.value];
+              updated.remove(credential);
+              selectedCredentials.value = updated;
+            },
+            child: const Icon(LucideIcons.trash, size: 16),
+          ),
+        ],
+      );
+    }
+
+    buildFormFields() {
+      final children = [];
+
+      for (int i = 0; i < selectedCredentials.value.length; i++) {
+        final data = selectedCredentials.value[i];
+        final allowedType = _AllowedCredentialType.values.firstWhere(
+          (e) => e.type == data.type,
+        );
+        children.add(switch (allowedType) {
+          .password => FTextFormField(
+            control: .managed(controller: data.dataController),
+            label: buildCredentialLabel(data),
+            minLines: 1,
+            obscureText: true,
+            autovalidateMode: .onUserInteraction,
+          ),
+        });
+      }
+
+      return children;
     }
 
     return Column(
       spacing: 16,
       children: [
-        for (final c in selectedCredentials.value.entries)
-          if (c.value.$1 == .password)
-            FCard(
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: .end,
-                    children: [buildRemoveCredentialButton(c.key)],
-                  ),
-                  FTextFormField(
-                    control: .lifted(
-                      value: TextEditingValue(text: c.value.$2),
-                      onChange: (v) => selectedCredentials.value = {
-                        ...selectedCredentials.value,
-                        c.key: (c.value.$1, v.text, c.value.$3),
-                      },
-                    ),
-                    label: Text('Password'),
-                    minLines: 1,
-                    obscureText: true,
-                    autovalidateMode: .onUserInteraction,
-                  ),
-                ],
-              ),
-            )
-          else if (c.value.$1 == .key) ...[
-            FCard(
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: .end,
-                    children: [buildRemoveCredentialButton(c.key)],
-                  ),
-                  FTextFormField(
-                    control: .lifted(
-                      value: TextEditingValue(text: c.value.$2),
-                      onChange: (v) => selectedCredentials.value = {
-                        ...selectedCredentials.value,
-                        c.key: (c.value.$1, v.text, c.value.$3),
-                      },
-                    ),
-                    label: Text('PEM Key'),
-                    hint: '-----BEGIN OPENSSH PRIVATE KEY-----',
-                    minLines: 5,
-                    maxLines: null,
-                    autovalidateMode: .onUserInteraction,
-                  ),
-                  const SizedBox(height: 12),
-                  FTextFormField(
-                    control: .lifted(
-                      value: TextEditingValue(text: c.value.$3 ?? ''),
-                      onChange: (v) => selectedCredentials.value = {
-                        ...selectedCredentials.value,
-                        c.key: (c.value.$1, c.value.$2, v.text),
-                      },
-                    ),
-                    label: Text('PEM Passphrase'),
-                    obscureText: true,
-                    maxLines: 1,
-                    autovalidateMode: .onUserInteraction,
-                  ),
-                ],
-              ),
-            ),
-          ],
+        ...buildFormFields(),
 
-        Wrap(
-          spacing: 16,
-          runSpacing: 8,
-          alignment: WrapAlignment.center,
-          children: [
-            FPopoverMenu(
-              menu: [
-                FItemGroup(
-                  children: [
-                    for (final allowed in _AllowedCredentialType.values)
-                      if (!allowed.singleInstance ||
-                          !selectedCredentials.value.values.any(
-                            (e) => e.$1 == allowed.type,
-                          ))
-                        FItem(
-                          prefix: Icon(allowed.icon),
-                          title: Text(allowed.label),
-                          onPress: () => selectedCredentials.value = {
-                            ...selectedCredentials.value,
-                            DateTime.now().millisecondsSinceEpoch: (
-                              allowed.type,
-                              '',
-                              null,
-                            ),
-                          },
-                        ),
-                  ],
-                ),
-              ],
-              builder: (context, controller, child) {
-                return FButton(
-                  style: FButtonStyle.secondary(),
-                  prefix: const Icon(LucideIcons.plus),
-                  onPress: controller.toggle,
-                  child: const Text('Add Credential'),
-                );
-              },
-            ),
-          ],
-        ),
+        // check if there are any allowed credential types left to add
+        if (_AllowedCredentialType.values.any(
+          (a) =>
+              !a.singleInstance ||
+              !selectedCredentials.value.any((e) => e.type == a.type),
+        ))
+          Wrap(
+            spacing: 16,
+            runSpacing: 8,
+            alignment: WrapAlignment.center,
+            children: [
+              FPopoverMenu(
+                menu: [
+                  FItemGroup(
+                    children: [
+                      for (final allowed in _AllowedCredentialType.values)
+                        if (!allowed.singleInstance ||
+                            !selectedCredentials.value.any(
+                              (e) => e.type == allowed.type,
+                            ))
+                          FItem(
+                            prefix: Icon(allowed.icon),
+                            title: Text(allowed.label),
+                            onPress: () {
+                              final updated = [
+                                ...selectedCredentials.value,
+                                _CredentialData(type: allowed.type),
+                              ];
+                              selectedCredentials.value = updated;
+                            },
+                          ),
+                    ],
+                  ),
+                ],
+                builder: (context, controller, child) {
+                  return FButton(
+                    style: FButtonStyle.ghost(),
+                    prefix: const Icon(LucideIcons.plus),
+                    onPress: controller.toggle,
+                    child: Flexible(
+                      child: const Text('Add Authentication Method'),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
       ],
     );
   }
