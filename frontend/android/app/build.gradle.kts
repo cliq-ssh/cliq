5fa5a2bf9a1626796ccfe4f1e7ddd602778ef258
Timon Klinkert <timon.klinkert@denux.dev>
2026-07-04 15:50:40 +0200

    chore(frontend): Rename package
    - `com.example` -> `sh.cliq`

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.cliq"
+    namespace = "sh.cliq.cliq"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.cliq"
+        applicationId = "sh.cliq.cliq"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- frontend/android/app/src/main/kotlin/com/example/cliq/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/example/cliq/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.cliq
+package sh.cliq.cliq
 
 import io.flutter.embedding.android.FlutterActivity
 
