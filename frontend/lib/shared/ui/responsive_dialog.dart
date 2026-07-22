9b99f0f4e7c6f0fe4d0e34d880f6d0409163b388
Jason Dean Lessenich <jasonlessenich@gmail.com>
2026-07-22 13:38:51 +0200

    add missing safearea for dialogs

--- frontend/lib/shared/ui/responsive_dialog.dart
+++ frontend/lib/shared/ui/responsive_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:cliq_ui/cliq_ui.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:forui/forui.dart' show FScaffold;
 
 class ResponsiveDialog extends StatelessWidget {
   static const double maxDesktopRatio = 9 / 16;
@@ -10,23 +11,28 @@ class ResponsiveDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        final size = constraints.biggest;
+    return FScaffold(
+      childPad: false,
+      child: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final size = constraints.biggest;
 
-        if (context.theme.breakpoints.getBreakpoint(size.width) >= .lg) {
-          final dialogWidth = size.width * maxDesktopRatio;
-          return ConstrainedBox(
-            constraints: BoxConstraints(
-              maxWidth: dialogWidth,
-              maxHeight: size.height,
-            ),
-            child: child,
-          );
-        }
+            if (context.theme.breakpoints.getBreakpoint(size.width) >= .lg) {
+              final dialogWidth = size.width * maxDesktopRatio;
+              return ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxWidth: dialogWidth,
+                  maxHeight: size.height,
+                ),
+                child: child,
+              );
+            }
 
-        return child;
-      },
+            return child;
+          },
+        ),
+      ),
     );
   }
 }
